ef39f99d58b45711a15566fc8c0cf30ff2f9dcd9
dongwook <boogiewooki02@gmail.com>
2026-05-04 15:40:46 +0900

    chore: Spring Boot 3.5.14 프로젝트 생성

---
+++ src/main/java/com/groupeat/GroupeatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.groupeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupeatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupeatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeat/GroupeatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupeatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

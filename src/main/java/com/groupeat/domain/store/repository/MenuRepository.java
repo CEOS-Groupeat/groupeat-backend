f6a7568bdbf4a5fa7b7f7a9309a6c00afdfff47a
Seungwon326 <sprjk0326@naver.com>
2026-05-19 20:45:11 +0900

    feat: 가게 메뉴 조회 기능 구현

---
+++ src/main/java/com/groupeat/domain/store/repository/MenuRepository.java
@@ -0,0 +1,15 @@
+package com.groupeat.domain.store.repository;
+
+import com.groupeat.domain.store.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("SELECT m FROM Menu m WHERE m.store.id = :storeId AND m.deletedAt IS NULL")
+    List<Menu> findAllByStoreId(@Param("storeId") Long storeId);
+
+}

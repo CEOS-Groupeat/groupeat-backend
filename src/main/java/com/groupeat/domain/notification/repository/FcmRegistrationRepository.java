ee93dc1837c3f9b749195df3e8b25750a72cda70
dongwook <boogiewooki02@gmail.com>
2026-07-13 02:23:13 +0900

    Feat: FCM 등록 엔티티 추가

---
+++ src/main/java/com/groupeat/domain/notification/repository/FcmRegistrationRepository.java
@@ -0,0 +1,15 @@
+package com.groupeat.domain.notification.repository;
+
+import com.groupeat.domain.notification.entity.FcmRegistration;
+import com.groupeat.domain.notification.enums.FcmPlatform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FcmRegistrationRepository extends JpaRepository<FcmRegistration, Long> {
+
+    Optional<FcmRegistration> findByRegistrationToken(String registrationToken);
+
+    List<FcmRegistration> findAllByMemberIdAndPlatformAndActiveTrue(Long memberId, FcmPlatform platform);
+}
